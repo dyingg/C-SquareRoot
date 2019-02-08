f1127d962a314922260ef8da46d4b605afae866d
dyingg <inktechser@gmail.com>
2019-02-08 19:08:16 +0530

    babylonian-naive

---
+++ babylonian-naive.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float babRoot(int n);
+
+int main(){
+    float r25 = babRoot(3);
+    printf("%f", r25);
+}
+
+
+
+float babRoot(int n) {
+
+
+
+    float root = n/2;
+    while( root * root != n && (root * root > ((float)n + 0.5) || root * root < ((float)n - 0.5)) ) {
+
+        root = ((n/root) + root)/2;
+
+    }
+    return root;
+}
+
+//Time 0.41s more accurate runtime  (?)
