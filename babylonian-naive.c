bf1e5ccc2a15b89e8907a238b34987ee6114c608
Dying <47161228+dyingg@users.noreply.github.com>
2019-11-26 03:09:13 +0530

    Update babylonian-naive.c

--- babylonian-naive.c
+++ babylonian-naive.c
@@ -7,19 +7,11 @@ int main(){
     printf("%f", r25);
 }
 
-
-
 float babRoot(int n) {
-
-
-
     float root = n/2;
     while( root * root != n && (root * root > ((float)n + 0.5) || root * root < ((float)n - 0.5)) ) {
-
         root = ((n/root) + root)/2;
-
     }
     return root;
 }
-
 //Time 0.41s more accurate runtime  (?)
